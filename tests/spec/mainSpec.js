7b0b9ea4c0c23be73637c6dc462d098a0629cac6
Rob Griffiths <rob@bytespider.eu>
2011-10-06 09:32:14 +0100

    Test authentication

--- tests/spec/mainSpec.js
+++ tests/spec/mainSpec.js
@@ -1,13 +1,37 @@
 var consumerKey;
 var consumerSecret;
+var successValue;
+var failureValue;
+
+function successCallback(data)
+{
+    console.log(data);
+    successValue = data;
+}
+
+function failiureCallback(data)
+{
+    failureValue = data;
+}
+
 
 describe("Twitter", function() {
-    var twitter;
+    var twitter = new OAuth.Twitter({
+        consumerKey: consumerKey,
+        consumerSecret: consumerSecret
+    });
     
-    beforeEach(function () {
-        twitter = new OAuth.Twitter({
-            consumerKey: consumerKey,
-            consumerSecret: consumerSecret
+    twitter.oauth.setCallbackUrl('oob');
+    
+    it('should authenticate a user', function () {
+        twitter.authenticate(successCallback, failiureCallback);
+        
+        waitsFor(function () {
+            return twitter.signedIn();
+        }, "user to authenticated", 30000);
+        
+        runs(function () {
+            expect(successValue).toBeDefined();
         });
-    })
+    }); 
 });
