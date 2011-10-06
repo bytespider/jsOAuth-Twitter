dcba89a8860703b6f204be1e0dd1d1976f09c616
Rob Griffiths <rob@bytespider.eu>
2011-10-06 09:30:51 +0100

    run the test chrome browser and launch our test runner

---
+++ tests/test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security --allow-file-access-from-files --allow-file-access --user-data-dir=chrome-test/ runner.html
