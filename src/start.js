f4a5ceee6c10226bb356a28780e596ec0e5938d4
Rob Griffiths <rob@bytespider.eu>
2011-10-10 08:59:52 +0100

    space

--- src/start.js
+++ src/start.js
@@ -4,6 +4,7 @@
  *  Copyright (c) 2011 Rob Griffiths (http://bytespider.eu)
  *  jsOAuth Twitter is freely distributable under the terms of an MIT-style license.
  */
+
 var exports = exports || this;
 exports.OAuth = (function (OAuth) {
 
