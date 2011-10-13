1e474fa759a33f21b649b7a45be3805710b86781
Rob Griffiths <rob@bytespider.eu>
2011-10-13 16:04:37 +0100

    updated tests

--- tests/spec/mainSpec.js
+++ tests/spec/mainSpec.js
@@ -1,37 +1,71 @@
 var consumerKey;
 var consumerSecret;
-var successValue;
-var failureValue;
-
-function successCallback(data)
-{
-    console.log(data);
-    successValue = data;
-}
-
-function failiureCallback(data)
-{
-    failureValue = data;
-}
-
 
 describe("Twitter", function() {
     var twitter = new OAuth.Twitter({
         consumerKey: consumerKey,
         consumerSecret: consumerSecret
     });
-    
-    twitter.oauth.setCallbackUrl('oob');
-    
+
     it('should authenticate a user', function () {
-        twitter.authenticate(successCallback, failiureCallback);
-        
+
+        var successCallback = jasmine.createSpy('success');
+        var failureCallback = jasmine.createSpy('failure');
+
+        twitter.authenticate(successCallback, failureCallback);
+
         waitsFor(function () {
-            return twitter.signedIn();
-        }, "user to authenticated", 30000);
-        
+            return successCallback.wasCalled || failureCallback.wasCalled;
+        }, "user to be authenticated", 30000);
+
         runs(function () {
-            expect(successValue).toBeDefined();
+            expect(successCallback).toHaveBeenCalled();
+            expect(failureCallback).not.toHaveBeenCalled();
         });
-    }); 
+    });
+
+    it("should show the user is logged in", function () {
+        expect(twitter.signedIn()).toBeTruthy();
+    });
+
+    it("should fetch the user's home timeline", function () {
+        var successCallback = jasmine.createSpy('success');
+        var failureCallback = jasmine.createSpy('failure');
+
+        twitter.homeTimeline(null, successCallback, failureCallback);
+
+        waitsFor(function () {
+            return successCallback.callCount > 0 || failureCallback.callCount > 0;
+        }, 60000);
+
+        runs(function () {
+            expect(successCallback).toHaveBeenCalled();
+            expect(failureCallback).not.toHaveBeenCalled();
+        });
+    });
+
+    var tweets = [
+        "jsOAuth http://cl.ly/33eC is a javscript OAuth librabry. Testing the Twitter module",
+        "jsOAuth has a new Twitter module http://cl.ly/AhyH",
+        "Testing jsOAuth, ignore me for a while",
+        "http://cl.ly/AhyH http://cl.ly/33eC jsOAuth rawks"
+    ];
+
+    var tweet = tweets[Math.floor(Math.random() * 4)];
+
+    it("should tweet \"" + tweet + "\"", function () {
+        var successCallback = jasmine.createSpy('success');
+        var failureCallback = jasmine.createSpy('failure');
+
+        twitter.tweet(tweet, successCallback, failureCallback);
+
+        waitsFor(function () {
+            return successCallback.callCount > 0 || failureCallback.callCount > 0;
+        }, 10000);
+
+        runs(function () {
+            expect(successCallback).toHaveBeenCalled();
+            expect(failureCallback).not.toHaveBeenCalled();
+        });
+    });
 });
